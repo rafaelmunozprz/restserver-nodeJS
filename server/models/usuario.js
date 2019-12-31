6d1d7707e9444824658458ef6b74f7cced4e564d
Rafael CRELO Software <rafaelmunoz.prz@gmail.com>
2019-12-31 15:09:41 -0600

    Capitulo 94

---
+++ server/models/usuario.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let usuarioSchema = new Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es necesario']
+    },
+    email: {
+        type: String,
+        required: [true, 'El email es necesario']
+    },
+    pass: {
+        type: String,
+        required: [true, 'La constrasena es obligatoria']
+    },
+    img: {
+        type: String,
+        required: false
+    },
+    role: {
+        type: String,
+        default: 'USER_ROLE'
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
--- server/server.js
+++ server/server.js
@@ -1,42 +1,25 @@
 require('./config/config');
 const express = require('express');
 const app = express();
+const color = require('colors');
+
+/**
+ * Conexion a mongoose
+ */
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/cafe',(error)=>{
+    if(error) throw Error;
+    console.log('Base de datos ONLINE'.green);
+});
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get('/usuario', (req, res)=>{
-    res.json('get Usuario')
-})
-
-app.post('/usuario', (req, res)=>{
-    let body = req.body;
-    if(body.nombre === undefined){
-        res.status(400).json({
-            error: {
-                ok: false,
-                mensaje: 'El nombre es necesario'
-            }
-        });
-    }else{
-        res.json({
-            persona: body
-        })
-    }    
-})
-
-app.put('/usuario/:id', (req, res)=>{
-    let id = req.params.id;
-    res.json({
-        id
-    })
-})
-
-app.delete('/usuario', (req, res)=>{
-    res.json('delete Usuario')
-})
-
+/**
+ * Archivo usuario importado al server
+ */
+app.use(require('./routes/usuario'));
 
 app.listen(process.env.PORT, ()=>{
     console.log('Escuchado puerto: ', process.env.PORT);
