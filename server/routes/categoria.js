fab67fc606b90fd1ed6dcff68005ea1ecc59af8e
Rafael-Desktop <rafaelmunoz.prz@gmail.com>
2020-01-09 08:59:49 -0600

    Video 142

---
+++ server/routes/categoria.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+let {verificaToken} = require('../middlewares/autenticacion');
+
+let app = express();
+
+let Catergoria = require('../models/categoria');
+
+/**
+ * GET TODAS LAS CATEGORIAS
+ */
+app.get('/categoria', (req,res)=>{
+
+});
+
+/**
+ * GET SOLO UN ID
+ */
+app.get('/categoria/:id', (req, res)=>{
+    Catergoria.findById();
+});
+
+/**
+ * POST: CREAR CATERGORIA
+ */
+app.post('/categoria', (req,res)=>{
+    //regresa lan ueva categoria
+    //req.usuario.id -> el id del usuario que creo la categoria
+
+});
+
+/**
+ * PUT: Actualizar datos de la categoria
+ */
+app.put('/categoria/:id', (req,res)=>{
+    //
+
+});
+
+/**
+ * DELETE: Solo permitido por un ADMIN-ROLE
+ */
+app.delete('/categoria/:id', (req,res)=>{
+
+});
+
+module.exports = app;
